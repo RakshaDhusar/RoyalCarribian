98ac33dbcbf8c3140e591ee7c69ac6b2fb71ab03
rd59140 <rd59140@MKD-CORP-BCP110.CORP.CYIENT.COM>
2021-06-09 19:12:31 +0530

    Test

--- src/test/java/com/cyient/test/SingUpTest.java
+++ src/test/java/com/cyient/test/SingUpTest.java
@@ -19,14 +19,14 @@ public class SingUpTest extends BrowserBase {
 		driver.findElement(By.xpath("//span[text()=' June ']")).click();
 		driver.findElement(By.xpath("//span[text()='Day']")).click();
 		driver.findElement(By.xpath("//span[text()=' 7 ']")).click();
-		driver.findElement(By.xpath("//input[@data-placeholder='Year']")).sendKeys("1997");
+		driver.findElement(By.xpath("//input[@data-placeholder='Year']")).sendKeys("1995");
 
 		driver.findElement(By.xpath("//span[text()='Country/Region of residence']")).click();
 		driver.findElement(By.xpath("//span[text()=' India ']")).click();
 		Thread.sleep(1000);
 
-		driver.findElement(By.xpath("//input[@data-placeholder='Email address']")).sendKeys("rutuk72@gmail.com");
-		driver.findElement(By.xpath("//input[@data-placeholder='Create new password']")).sendKeys("Feb27@rak");
+		driver.findElement(By.xpath("//input[@data-placeholder='Email address']")).sendKeys("RD@gmail.com");
+		driver.findElement(By.xpath("//input[@data-placeholder='Create new password']")).sendKeys("SK@07");
 
 		driver.findElement(By.xpath("(//span[text()='Select one security question'])[1]")).click();
 		Thread.sleep(1000);
